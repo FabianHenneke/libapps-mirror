b22c600e5c4f58fe68cda54e660d3b4b7c993e0d
Fabian Henneke <fabian@henneke.me>
2019-02-16 15:34:58 +0100

    Fix: Export ECPublicKey from pkijs
    Change-Id: Icbe5a81ba4ebaacde07b1910c3d79063d23d2ff2

--- nassh/js/nassh_deps_rollup_wrapper.js
+++ nassh/js/nassh_deps_rollup_wrapper.js
@@ -2,6 +2,6 @@ import { fromBER } from 'asn1js';
 const asn1js = { fromBER };
 export { asn1js };
 
-import { Certificate, RSAPublicKey } from 'pkijs';
-const pkijs = { Certificate, RSAPublicKey };
+import { Certificate, ECPublicKey, RSAPublicKey } from 'pkijs';
+const pkijs = { Certificate, ECPublicKey, RSAPublicKey };
 export { pkijs };
